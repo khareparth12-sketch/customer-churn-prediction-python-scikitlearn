e248d4f1082ddda81751282e428d50679d2796dd
ParthKhare21 <khare.parth2112@gmail.com>
2026-03-22 22:17:08 +0530

    Fixed Bug. Docker Container running smoothly

--- dashboard/app.py
+++ dashboard/app.py
@@ -208,7 +208,7 @@ with tab2:
         }
 
         try:
-            response = requests.post("http://0.0.0.0:8000/predict", json=payload)
+            response = requests.post("http://api:8000/predict", json=payload)
 
             result = response.json()
 
